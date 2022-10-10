71f301eb8e02020b0682b820f51405a4e822dcae
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 00:47:20 +0600

    Toast Working fine with right and wrong answer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import parse from 'html-react-parser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ loadedQuestion }) => {
   const { correctAnswer, question, options } = loadedQuestion;
 
   const handleCorrectAnswer = (event) => {
-    console.log(event.target.textContent.trim())
-    console.log(correctAnswer)
-    console.log(event.target.textContent.trim() === correctAnswer)
+    if (event.target.textContent.trim() === correctAnswer) {
+      toast.success('Right Answer');
+    }
+    else {
+      toast.warn('Wrong Answer');
+    }
+  };
+
+  const handleShowAnswer = () => {
+    toast(correctAnswer);
   }
+
+
   return (
     <div>
+      <button onClick={handleShowAnswer}>Show Right Answer</button>
       {parse(question)}
       {options.map((option, index) => <div onClick={handleCorrectAnswer} key={index}> {option} </div>)}
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 };
