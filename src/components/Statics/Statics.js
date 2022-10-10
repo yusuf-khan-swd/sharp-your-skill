96d08a43532501fb9c555ccf6085154bbe56af60
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 01:18:19 +0600

    add more rechart component

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const Statics = () => {
   const data = useLoaderData().data;
   return (
-    <div>
+    <div >
       <h2>This is static</h2>
       <LineChart width={900} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
         <Tooltip></Tooltip>
+        <Legend />
+        <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
       </LineChart>
     </div>
   );
