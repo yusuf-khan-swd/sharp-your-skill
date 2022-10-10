22b81dc054d0480be8f8029d456c8be8d78f95ad
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 00:19:35 +0600

    add handler for correct answer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,9 +3,16 @@ import parse from 'html-react-parser';
 
 const Question = ({ loadedQuestion }) => {
   const { correctAnswer, question, options } = loadedQuestion;
+
+  const handleCorrectAnswer = (event) => {
+    console.log(event.target.textContent.trim())
+    console.log(correctAnswer)
+    console.log(event.target.textContent.trim() === correctAnswer)
+  }
   return (
     <div>
       {parse(question)}
+      {options.map((option, index) => <div onClick={handleCorrectAnswer} key={index}> {option} </div>)}
     </div>
   );
 };
