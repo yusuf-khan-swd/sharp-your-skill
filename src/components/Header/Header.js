383f441ac929b1f67c3b3da296a073b590591ab8
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 17:55:25 +0600

    remove style from nav for link not work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
         <img className="w-10 h-10 mr-3" src={logo} alt="" />
         <p className="text-3xl">Sharp Your Skill</p>
       </Link>
-      <nav className="text-lg">
+      <nav>
         <NavLink className={`mr-3 hover:text-zinc-400`} to="/home">
           Home
         </NavLink>
