a405b31f241e44fbfb42e1ddb6996ccfe621f55e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 18:12:55 +0600

    format with prettier

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statics = () => {
   const data = useLoaderData().data;
   return (
-    <div className='mb-28'>
-      <h2 className='text-3xl font-bold mb-8 text-center'>Total <span className='text-red-500'> {data.length} </span> Question Statics </h2>
-      <div className='w-4/5 h-96 mx-auto'>
+    <div className="mb-28">
+      <h2 className="text-3xl font-bold mb-8 text-center">
+        Total <span className="text-red-500"> {data.length} </span> Question
+        Statics{" "}
+      </h2>
+      <div className="w-4/5 h-96 mx-auto">
         <ResponsiveContainer width="100%" height="100%">
           <LineChart data={data}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -15,7 +27,12 @@ const Statics = () => {
             <YAxis />
             <Tooltip></Tooltip>
             <Legend />
-            <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
+            <Line
+              type="monotone"
+              dataKey="total"
+              stroke="#8884d8"
+              strokeWidth={2}
+            />
           </LineChart>
         </ResponsiveContainer>
       </div>
