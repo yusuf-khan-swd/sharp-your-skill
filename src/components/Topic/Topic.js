dcb631157d279a5113d50136d10b5cddc95adb5a
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 09:32:43 +0600

    Make Responsive Topics Card

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Topic = ({ topic }) => {
   const { id, logo, name, total } = topic;
   return (
-    <div className='shadow-lg border rounded-lg'>
-      <img className='h-32 w-32 rounded-lg m-2 border' src={logo} alt="" />
+    <div className='shadow-lg border rounded-lg pb-2 w-60 mx-auto'>
+      <img className='h-56 rounded-lg m-2 border' src={logo} alt="" />
       <div className='ml-2 mb-2'>
-        <h5 className='font-bold text-xl mb-2'> {name} </h5>
-        <p className='mb-2'>Total Quiz: {total} </p>
+        <h5 className='font-bold text-3xl mb-2'> {name} </h5>
+        <p className='mb-2 text-slate-400 text-xl'>Total Quiz: {total} </p>
         <Link to={`/topic/${id}`}>
-          <button className='bg-zinc-400 py-1 px-2 rounded-md'>Start Practice</button>
+          <button className='bg-zinc-400 hover:bg-zinc-500 py-1 px-2 text-2xl rounded-md'>Start Practice</button>
         </Link>
       </div>
       <div>
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -5,7 +5,7 @@ import Topic from '../Topic/Topic';
 const Topics = () => {
   const topics = useLoaderData().data;
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-8'>
       {topics.map(topic => <Topic key={topic.id} topic={topic}></Topic>)}
     </div>
   );
