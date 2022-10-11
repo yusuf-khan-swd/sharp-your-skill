53b7b3b1d3657e87892ba27d0df4fc770262a74b
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 22:33:15 +0600

    format code and remove comment

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,9 +1,9 @@
-import parse from 'html-react-parser';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
-import Options from '../Options/Options';
+import parse from "html-react-parser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import Options from "../Options/Options";
 
 const Question = ({ loadedQuestion }) => {
   const { correctAnswer, question, options } = loadedQuestion;
@@ -11,36 +11,39 @@ const Question = ({ loadedQuestion }) => {
   const handleCorrectAnswer = (event) => {
     const selected = event.target.value;
     if (selected) {
-
       if (selected === correctAnswer) {
-        toast.success('Right Answer');
-      }
-      else {
-        toast.warn('Wrong Answer!');
+        toast.success("Right Answer");
+      } else {
+        toast.warn("Wrong Answer!");
       }
     }
-    // if (event.target.textContent.trim() === correctAnswer) {
-    //   toast.success('Right Answer');
-    // }
-    // else {
-    //   toast.warn('Wrong Answer');
-    // }
   };
 
   const handleShowAnswer = () => {
     toast.info(correctAnswer);
-  }
+  };
 
   return (
-    <div className='border shadow-md p-4 rounded-lg'>
-      <div className='flex justify-between items-center'>
+    <div className="border shadow-md p-4 rounded-lg">
+      <div className="flex justify-between items-center">
         {parse(question)}
-        <button className='m-3' title='show the right answer' onClick={handleShowAnswer}>
+        <button
+          className="m-3"
+          title="show the right answer"
+          onClick={handleShowAnswer}
+        >
           <FontAwesomeIcon icon={faEye} />
         </button>
       </div>
       <div>
-        {options.map((option, index) => <Options handleCorrectAnswer={handleCorrectAnswer} question={question} key={index} option={option}></Options>)}
+        {options.map((option, index) => (
+          <Options
+            handleCorrectAnswer={handleCorrectAnswer}
+            question={question}
+            key={index}
+            option={option}
+          ></Options>
+        ))}
       </div>
       <ToastContainer
         position="top-right"
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Question from '../Question/Question';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Question from "../Question/Question";
 
 const Quiz = () => {
   const quiz = useLoaderData().data;
@@ -12,9 +12,11 @@ const Quiz = () => {
         src={logo}
         alt=""
       />
-      <h2 className='text-center text-5xl font-semibold'>{name} Quiz</h2>
-      <div className='max-w-lg grid gap-5 mx-auto mt-8 mb-28'>
-        {questions.map(question => <Question key={question.id} loadedQuestion={question}></Question>)}
+      <h2 className="text-center text-5xl font-semibold">{name} Quiz</h2>
+      <div className="max-w-lg grid gap-5 mx-auto mt-8 mb-28">
+        {questions.map((question) => (
+          <Question key={question.id} loadedQuestion={question}></Question>
+        ))}
       </div>
     </div>
   );
