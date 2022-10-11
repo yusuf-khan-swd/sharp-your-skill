3b847a570c68c9ea63b91784e29a0be036e677d9
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 18:12:33 +0600

    add heading to statics

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -5,17 +5,20 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 const Statics = () => {
   const data = useLoaderData().data;
   return (
-    <div className='w-4/5 h-96 mx-auto'>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip></Tooltip>
-          <Legend />
-          <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
-        </LineChart>
-      </ResponsiveContainer>
+    <div className='mb-28'>
+      <h2 className='text-3xl font-bold mb-8 text-center'>Total <span className='text-red-500'> {data.length} </span> Question Statics </h2>
+      <div className='w-4/5 h-96 mx-auto'>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip></Tooltip>
+            <Legend />
+            <Line type="monotone" dataKey="total" stroke="#8884d8" strokeWidth={2} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
