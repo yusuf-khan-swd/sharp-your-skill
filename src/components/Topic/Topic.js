c4b6a2d907da67217aaa35ae48a7d73e07b37fb5
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 15:50:31 +0600

    modified style in topic cart

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Topic = ({ topic }) => {
   const { id, logo, name, total } = topic;
   return (
-    <div className='shadow-lg border rounded-lg pb-2 w-60 mx-auto'>
-      <img className='h-56 rounded-lg m-2 border' src={logo} alt="" />
+    <div className='shadow-lg border rounded-lg pb-5 mx-auto text-center'>
+      <img className='w-11/12 mx-auto mt-3 mb-3 rounded-lg border' src={logo} alt="" />
       <div className='ml-2 mb-2'>
-        <h5 className='font-bold text-3xl mb-2'> {name} </h5>
-        <p className='mb-2 text-slate-400 text-xl'>Total Quiz: {total} </p>
+        <h5 className='font-bold text-3xl mb-3'> {name} </h5>
+        <p className='mb-3 text-slate-400 text-xl'>Total Quiz: {total} </p>
         <Link to={`/topic/${id}`}>
           <button className='bg-zinc-400 hover:bg-zinc-500 py-1 px-2 text-2xl rounded-md'>Start Practice</button>
         </Link>
