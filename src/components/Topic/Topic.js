f0f3877ffc8c6821b5a443f920beeba36f22a3be
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 09:08:15 +0600

    added style to card of topics

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Topic = ({ topic }) => {
   const { id, logo, name, total } = topic;
   return (
-    <div>
-      <img className='h-32 w-32' src={logo} alt="" />
-      <div>
-        <h5> {name} </h5>
-        <p>Total Quiz: {total} </p>
+    <div className='shadow-lg border rounded-lg'>
+      <img className='h-32 w-32 rounded-lg m-2 border' src={logo} alt="" />
+      <div className='ml-2 mb-2'>
+        <h5 className='font-bold text-xl mb-2'> {name} </h5>
+        <p className='mb-2'>Total Quiz: {total} </p>
         <Link to={`/topic/${id}`}>
-          <button className='bg-zinc-400 py-1 px-2'>Start Practice</button>
+          <button className='bg-zinc-400 py-1 px-2 rounded-md'>Start Practice</button>
         </Link>
       </div>
       <div>
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -5,7 +5,7 @@ import Topic from '../Topic/Topic';
 const Topics = () => {
   const topics = useLoaderData().data;
   return (
-    <div>
+    <div className='container mx-auto'>
       {topics.map(topic => <Topic key={topic.id} topic={topic}></Topic>)}
     </div>
   );
