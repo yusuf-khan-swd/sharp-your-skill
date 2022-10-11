ce5195d3968d901e8ad2ea62e13072f0ac5896e9
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 17:47:11 +0600

    added quiz name and logo

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,10 +4,18 @@ import Question from '../Question/Question';
 
 const Quiz = () => {
   const quiz = useLoaderData().data;
-  const { questions } = quiz;
+  const { questions, logo, name } = quiz;
   return (
-    <div className='max-w-lg grid gap-5 mx-auto mt-8 mb-28'>
-      {questions.map(question => <Question key={question.id} loadedQuestion={question}></Question>)}
+    <div>
+      <img
+        className="w-24 mx-auto mt-3 mb-3 rounded-lg border bg-slate-500"
+        src={logo}
+        alt=""
+      />
+      <h2 className='text-center text-5xl font-semibold'>{name} Quiz</h2>
+      <div className='max-w-lg grid gap-5 mx-auto mt-8 mb-28'>
+        {questions.map(question => <Question key={question.id} loadedQuestion={question}></Question>)}
+      </div>
     </div>
   );
 };
