837be724ad439ada69abc220d36c9cea140cf1d3
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 15:52:56 +0600

    center statics

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -5,7 +5,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 const Statics = () => {
   const data = useLoaderData().data;
   return (
-    <div className='w-4/5 h-96'>
+    <div className='w-4/5 h-96 mx-auto'>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
