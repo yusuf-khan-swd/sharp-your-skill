97273fef1f3c22ab851063bce34a02ba8b351c36
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-11 11:42:26 +0600

    added style to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='flex flex-col text-center'>
+    <div className='flex flex-col md:flex-row justify-between bg-gray-900 text-white items-center text-center p-5 mb-24'>
       <h1 className='text-3xl'>Sharp Your Skill</h1>
       <nav>
         <NavLink className={`mr-3 hover:text-zinc-400`} to='/home'>Home</NavLink>
