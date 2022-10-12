180c523617ce85c14766b38179f75e9140100ce9
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-12 16:33:50 +0600

    added container to navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,28 +5,30 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between bg-gray-900 text-white items-center text-center p-5">
-      <Link
-        to={`/`}
-        className="flex mb-3 flex-col md:flex-row justify-center items-center"
-      >
-        <img className="w-10 h-10 mr-3" src={logo} alt="" />
-        <p className="text-3xl">Sharp Your Skill</p>
-      </Link>
-      <nav>
-        <NavLink className={`mr-3 hover:text-zinc-400 ${({ isActive }) => isActive ? 'active' : undefined}`} to="/home">
-          Home
-        </NavLink>
-        <NavLink className={`mr-3 hover:text-zinc-400`} to="/topics">
-          Quiz Topics
-        </NavLink>
-        <NavLink className={`mr-3 hover:text-zinc-400`} to="/statistics">
-          Statistics
-        </NavLink>
-        <NavLink className={`mr-3 hover:text-zinc-400`} to="/blog">
-          Blog
-        </NavLink>
-      </nav>
+    <div className="bg-gray-900 text-white">
+      <div className="flex flex-col md:flex-row justify-between items-center text-center p-5 container mx-auto">
+        <Link
+          to={`/`}
+          className="flex mb-3 flex-col md:flex-row justify-center items-center"
+        >
+          <img className="w-10 h-10 mr-3" src={logo} alt="" />
+          <p className="text-3xl">Sharp Your Skill</p>
+        </Link>
+        <nav>
+          <NavLink className={`mr-3 hover:text-zinc-400 ${({ isActive }) => isActive ? 'active' : undefined}`} to="/home">
+            Home
+          </NavLink>
+          <NavLink className={`mr-3 hover:text-zinc-400`} to="/topics">
+            Quiz Topics
+          </NavLink>
+          <NavLink className={`mr-3 hover:text-zinc-400`} to="/statistics">
+            Statistics
+          </NavLink>
+          <NavLink className={`mr-3 hover:text-zinc-400`} to="/blog">
+            Blog
+          </NavLink>
+        </nav>
+      </div>
     </div>
   );
 };
